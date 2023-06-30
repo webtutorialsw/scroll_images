6fa2be1bbb2a960b3f85c7d253dc94a0e9d1336b
jamwag <james.wagner@outlook.de>
2023-06-30 11:16:43 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Stunning Scroll Animation with GSAP
+
+![Thumbnail](thumbnail.png)
+
+Full tutorial here: https://www.youtube.com/watch?v=kdVu4eFgKkY
+
+Images (not in repo): https://wirestock.io/james591
---
+++ script.js
@@ -0,0 +1,24 @@
+var items = document.querySelectorAll(".item img");
+
+items.forEach((val, index) => {
+    val.src = "./images/" + (index + 1) + ".png";
+});
+
+
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".wrapper",
+        pin: true,
+        scrub: 2,
+        start: "top top",
+        end: "50%+=500px",
+    }
+});
+
+tl.to(".items img", {scale: 1}, 0);
+tl.to(".items", {scale: 2, rotate: 0}, 0);
+
+
+tl.to(".overlay", {height: "100%"}, .2);
+tl.to(".overlay h1", {scale: 1}, .6);
+tl.to(".items", {scale: .8, opacity: .2}, .6);
